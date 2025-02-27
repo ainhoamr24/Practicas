2b6ee1c7cf8de777ba31f17bfd8b5120949517dc
Luis Miguel Olmeda Romero <olmedaromeroluismiguel@gmail.com>
2025-02-27 10:39:31 +0100

    Primer commit

---
+++ src/main/java/com/fpmislata/daw1/SoundE/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.fpmislata.daw1.SoundE.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    public MainController() {
+    }
+
+    @SuppressWarnings("SameReturnValue")
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+}
