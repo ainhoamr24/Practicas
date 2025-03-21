4089873b50b1f3170c30b36d9f9688ceeff0b125
Javier <150188126+ilygomzzz@users.noreply.github.com>
2025-03-21 13:28:49 +0100

    Deleted exceptions (#118)

--- src/main/java/com/fpmislata/daw1/SoundE/domain/service/impl/SongServiceImpl.java
+++ src/main/java/com/fpmislata/daw1/SoundE/domain/service/impl/SongServiceImpl.java
@@ -15,34 +15,22 @@ public class SongServiceImpl implements SongService {
     }
 
     @Override
-    public Song findById(Long id) throws SongException{
-        Song song = songRepository.findById(id);
-        if(song == null)
-            throw new SongException("La cancion con id: " + id + " no existe");
-        return song;
+    public Song findById(Long id) {
+        return songRepository.findById(id);
     }
 
     @Override
-    public List<Song> findByName(String name) throws SongException {
-        List<Song> songs = songRepository.findByName(name);
-        if(songs.isEmpty())
-            throw new SongException("No se ha encontrado ninguna cancion con el nombre: " + name);
-        return songs;
+    public List<Song> findByName(String name) {
+        return songRepository.findByName(name);
     }
 
     @Override
-    public List<Song> findByGenre(String genre) throws SongException {
-        List<Song> songs = songRepository.findByGenre(genre);
-        if(songs.isEmpty())
-            throw new SongException("No se ha encontrado ninguna canción con el genero: " + genre);
-        return songs;
+    public List<Song> findByGenre(String genre) {
+        return songRepository.findByGenre(genre);
     }
 
     @Override
-    public List<Genre> getGenresBySong(Song song) throws SongException {
-        List<Genre> genres = songRepository.getGenresBySong;
-        if(genres.isEmpty())
-            throw new SongException("La cancion: " + song.getName() + "no contiene ningun genero");
-        return genres;
+    public List<Genre> getGenresBySong(Song song) {
+        return songRepository.getGenresBySong;
     }
 }
