1c3d7325dcb1280e2a8e545d9d5b3b9f850ac3e3
Javier <150188126+ilygomzzz@users.noreply.github.com>
2025-03-31 09:49:52 +0200

    Añadida la conexión a la base de datos (#199)

---
+++ src/main/java/com/fpmislata/daw1/SoundE/persistance/dao/impl/jdbc/database/DatabaseConnection.java
@@ -0,0 +1,50 @@
+package com.fpmislata.daw1.SoundE.persistance.dao.impl.jdbc.database;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.sql.*;
+import java.util.List;
+
+@Log4j2
+public class DatabaseConnection {
+    private static DatabaseConnection instance = null;
+    public static DatabaseConnection getInstance() {
+        if(instance == null)
+            instance = new DatabaseConnection();
+
+        return instance;
+    }
+
+    private final String URL;
+    private final String USER;
+    private final String PASSWORD;
+    private final Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    private DatabaseConnection() {
+        URL = "jdbc:mysql://localhost:3306/sounde";
+        USER = "sounde";
+        PASSWORD = "sounde";
+
+        log.info("Estableciendo conexión con la base de datos...");
+        try {
+            connection = DriverManager.getConnection(URL,USER,PASSWORD);
+            log.info("Conexión establecida");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ResultSet executeSql(String sql, List<Object> parameters) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        for (int i = 0; i < parameters.size(); i++) {
+            preparedStatement.setObject(i + 1, parameters.get(i));
+        }
+
+        return preparedStatement.executeQuery();
+    }
+}
