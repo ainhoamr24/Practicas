77ed7d4f73ba2b07243ca433ab9719d34fb106cc
Javier <150188126+ilygomzzz@users.noreply.github.com>
2025-04-02 12:43:56 +0200

    Arreglados atributos de la consulta (#226)

--- src/main/java/com/fpmislata/daw1/SoundE/persistance/dao/impl/jdbc/PlaylistSongDaoJdbc.java
+++ src/main/java/com/fpmislata/daw1/SoundE/persistance/dao/impl/jdbc/PlaylistSongDaoJdbc.java
@@ -20,11 +20,11 @@ public class PlaylistSongDaoJdbc implements PlaylistSongDao {
 
     @Override
     public List<Playlist> findByGenre(String name) {
-        String sql = "SELECT p.id_playlist, p.name, p.dateCreate, p.imagePath, p.artist, p.artistImgPath " +
+        String sql = "SELECT p.id_playlist, p.name, p.dateCreate, p.imgPath, p.artist, p.artistImgPath " +
                 "FROM tb_playlist p " +
-                "INNER JOIN tb_playlistsong ps ON p.playlist_id = ps.pls_id_playlist " +
+                "INNER JOIN tb_playlistsong ps ON p.id_playlist = ps.pls_id_playlist " +
                 "INNER JOIN tb_song s ON ps.pls_id_song = s.id_song " +
-                "INNER JOIN tb_songgenres sg ON s.id_song = sg.sgr_id_song" +
+                "INNER JOIN tb_songgenres sg ON s.id_song = sg.sgr_id_song " +
                 "INNER JOIN tb_genre g ON g.id_genre = sg.sgr_id_genre " +
                 "WHERE g.name = ?";
 
